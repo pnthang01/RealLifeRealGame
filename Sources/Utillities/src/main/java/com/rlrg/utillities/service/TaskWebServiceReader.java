75a24fbfa9ab8ab6d9d9def447415a5427f9ca02
Phuong Truong <ptruong5260@westfield.ma.edu>
2014-06-25 14:20:37 -0400

    add TaskWebServiceReader

---
+++ Sources/Utillities/src/main/java/com/rlrg/utillities/service/TaskWebServiceReader.java
@@ -0,0 +1,60 @@
+package com.rlrg.utillities.service;
+
+import java.util.List;
+
+import org.springframework.web.client.RestClientException;
+
+import com.rlrg.dataserver.task.dto.TaskDTO;
+import com.rlrg.utillities.exception.ConvertException;
+
+public class TaskWebServiceReader extends BaseWebServiceReader<TaskDTO> {
+	
+	private final String MODULE_NAME = "TaskModule";
+	
+	private final String GET_ALL_TASKS_BY_STATUS_URL = "task/getTasksByStatus?status={status}&pageNumber={pageNumber}";
+	private final String GET_ALL_TASKS_URL = "task/getAllTasks?pageNumber={pageNumber}";
+	private final String GET_TASK_BY_ID_URL = "task/getTaskByID?id={id}";
+	private final String GET_ALL_TASKS_BY_USER_ID_URL = "task/getAllTasksByUserID?userId={userId}";
+	private final String GET_ALL_TASKS_BY_LEVEL_URL = "task/getAllTasksByLevel?difficultyLevel={difficultyLevel}";
+	private final String GET_ALL_TASKS_BY_POINT_URL = "task/getAllTasksByPoint?point={point}";
+	private final String SEARCH_TASKS_BY_KEYWORD_URL = "task/searchTasks?keyword={keyword}&pageNumber={pageNumber}";
+	private final String UPDATE_TASK_URL = "task/update?restobject={restobject}";
+	
+	public List<TaskDTO> getTasksByStatus(boolean status, Integer pageNumber) throws RestClientException, ConvertException{
+		return this.getListOfObjects(GET_ALL_TASKS_BY_STATUS_URL, MODULE_NAME, status, pageNumber);
+	}
+	
+	public List<TaskDTO> getAllTasks(Integer pageNumber) throws RestClientException, ConvertException{
+		return this.getListOfObjects(GET_ALL_TASKS_URL, MODULE_NAME, pageNumber);
+	}
+	
+	public TaskDTO getTaskById(Long id) throws RestClientException, ConvertException{
+		return this.getAnObject(GET_TASK_BY_ID_URL, MODULE_NAME, id);
+	}
+	
+	public List<TaskDTO> getTasksByUserId(Long userId) throws RestClientException, ConvertException{
+		return this.getListOfObjects(GET_ALL_TASKS_BY_USER_ID_URL, MODULE_NAME, userId);
+	}
+	
+	public List<TaskDTO> getTasksByLevel(Integer difficultyLevel) throws RestClientException, ConvertException{
+		return this.getListOfObjects(GET_ALL_TASKS_BY_LEVEL_URL, MODULE_NAME, difficultyLevel);
+	}
+	
+	public List<TaskDTO> getTasksByPoint(Integer point) throws RestClientException, ConvertException{
+		return this.getListOfObjects(GET_ALL_TASKS_BY_POINT_URL, MODULE_NAME, point);
+	}
+	
+	public List<TaskDTO> searchTasksByKeyword(String keyword, Integer pageNumber) throws RestClientException, ConvertException{
+		return this.getListOfObjects(SEARCH_TASKS_BY_KEYWORD_URL, MODULE_NAME, keyword, pageNumber);
+	}
+	
+	public boolean updateTask(TaskDTO dto) throws RestClientException, ConvertException{
+		return this.postAnObjectT(UPDATE_TASK_URL, MODULE_NAME, dto);
+	}
+
+	@Override
+	protected Class<TaskDTO> getTClass() {
+		return TaskDTO.class;
+	}
+
+}
