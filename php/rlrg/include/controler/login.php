f616e142138cb7f560a27d38f2aacb66538bd928
anhdt123 <anhdt@seatechit.com.vn>
2014-08-11 19:53:09 +0700

    test
    ok

--- php/rlrg/include/controler/login.php
+++ php/rlrg/include/controler/login.php
@@ -1,11 +1,11 @@
 <?php
 	include '../model/login_service.php';
 	$user = new UserVO();
-
-	$user->setUsername("testacc");
-	$user->setPassword("123456");
-	
-	$userSV = new UserService();
-	$userSV->exeLogin($user);
-
-	
+	if(isset($_POST['user']) || isset($_POST['pass'])){
+		$user->setUsername($_POST['user']);
+		$user->setPassword($_POST['pass']);
+		$userSV = new UserService();
+		$userSV->exeLogin($user);
+	} else {
+		echo ERROR_SERVER;
+	}
